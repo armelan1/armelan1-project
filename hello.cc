a09b4b0dec399c0b9e7ad550d86bfdaf238a772b
Austin Melancon <armelan1@asu.edu>
2025-11-16 14:49:44 -0700

    added iterator for loop in hello.cc

--- hello.cc
+++ hello.cc
@@ -1,7 +1,14 @@
 #include <iostream>
+#include <vector>
 
 int main() 
 {
+    std::vector<int> myVector = {1,2,3,5,8,13,21};
     std::cout << "Hello, World!" << std::endl;
+
+    for (auto i : myVector)
+    {
+        std::cout << i << std::endl;
+    }
     return 0;
 }
